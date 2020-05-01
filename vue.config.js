77d2dd49b33ece8085962d9aaee9ea92126a4b91
happyPeiLin <18398322316@163.com>
2020-05-02 02:07:40 +0800

    初始化，并配置vue.config

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    devServer:{
+        port:1357,
+        host:'localhost',
+        https:false,
+        open:true,
+    },
+    lintOnSave:false,
+    productionSourceMap:false,
+}
